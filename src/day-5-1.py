a05550694a21fef581c8c183b54fbbd9ea1c2483
Xiong Jingya <66959387+xiongjya@users.noreply.github.com>
2021-12-05 16:32:09 +0800

    Add day 5 program solutions

--- src/day-5-1.py
+++ src/day-5-1.py
@@ -3,7 +3,8 @@ lines = file.readlines()
 
 x_max = 0   # maximum x-coordinate / row
 y_max = 0   # maximum y-coordinate / column
-coords = []
+straight_coords = []    # stores horizontal and vertical coords
+diagonal_coords = []    # stores diagonal coords
 
 
 def mark_straight(is_vertical, constant, n, m, map):
@@ -23,7 +24,9 @@ for line in lines:
     coord[1] = coord[1].split(",")
 
     if int(coord[0][0]) == int(coord[1][0]) or int(coord[0][1]) == int(coord[1][1]):
-        coords.append(coord)
+        straight_coords.append(coord)
+    else:
+        diagonal_coords.append(coord)
 
     x_max = max(max(int(coord[0][0]), int(coord[1][0])), x_max)
     y_max = max(max(int(coord[0][1]), int(coord[1][1])), y_max)
@@ -35,13 +38,32 @@ map = [0] * y_max
 for i in range(0, y_max):
     map[i] = [0] * x_max
 
-for coord in coords:
+for coord in straight_coords:
     is_vertical = int(coord[0][0]) - int(coord[1][0]) == 0
 
     if is_vertical:
         mark_straight(is_vertical, int(coord[0][0]), int(coord[0][1]), int(coord[1][1]), map)
     else:
         mark_straight(is_vertical, int(coord[0][1]), int(coord[0][0]), int(coord[1][0]), map)
+
+for coord in diagonal_coords:
+    is_first = int(coord[0][0]) < int(coord[1][0])
+    difference = abs(int(coord[0][0]) - int(coord[1][0])) + 1
+
+    if not is_first:
+        temp = coord[0]
+        coord[0] = coord[1]
+        coord[1] = temp
+
+    x_start = int(coord[0][0])
+    y_start = int(coord[0][1])
+    is_y_increase = int(coord[0][1]) - int(coord[1][1]) < 0
+
+    for i in range(0, difference):
+        if is_y_increase:
+            map[y_start + i][x_start + i] += 1
+        else:
+            map[y_start - i][x_start + i] += 1
 
 result = 0
 for r in range(0, y_max):
